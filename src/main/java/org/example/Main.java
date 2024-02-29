5603742ea03204f183a894b1f66bf37e44ea3ff6
Jsaulbautista <150806077+Jsaulbautista@users.noreply.github.com>
2024-02-29 01:15:07 -0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("ingresar calificacion");
+        frame.setContentPane(new ingresarDatos().ingresarDatosPanel);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(800,300);
+        frame.setVisible(true);
+
+    }
+}
---
+++ src/main/java/org/example/ingresarDatos.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ingresarDatos {
+    private JTextField nombreField;
+    private JTextField apellidoField;
+    private JButton ingresarDatos;
+    public JPanel ingresarDatosPanel;
+
+    public ingresarDatos() {
+        ingresarDatos.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MongoClient mongoClient = MongoClients.create("mongodb+srv://saul:12345654321@saul.ey7q2vo.mongodb.net/?retryWrites=true&w=majority&appName=Saul");
+
+
+                MongoDatabase database = mongoClient.getDatabase("ConexionJava");
+                MongoCollection<Document> collection = database.getCollection("usuarios");
+
+                Document document = new Document("name", nombreField.getText())
+                        .append("apellido", apellidoField.getText());
+
+                collection.insertOne(document);
+                System.out.println("Documento insertado");
+
+                JFrame salIns = new JFrame("Mensaje");
+                JOptionPane.showMessageDialog(salIns, "Datos ingresados correctamente");
+            }
+        });
+    }
+}
